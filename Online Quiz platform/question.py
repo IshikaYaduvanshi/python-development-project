7f578ae675ff4808c0bff33a802da0f59874d7b9
ISHIKA <ishikaudvanshi@gmail.com>
2026-05-15 20:21:55 +0530

    Add files via upload

---
+++ Online Quiz platform/question.py
@@ -0,0 +1,39 @@
+quiz_data = [
+    {
+        "question": "What is the capital of India?",
+        "options": ["Mumbai", "New Delhi", "Kolkata", "Chennai"],
+        "answer": "Delhi"
+    },
+    {
+        "question": "Which language is used for Data Analysis?",
+        "options": ["Python", "HTML", "CSS", "JavaScript"],
+        "answer": "Python"
+    },
+    {
+        "question": "What does SQL stand for?",
+        "options": [
+            "Structured Query Language", "Simple Query Language","Sequential Query Language","Standard Question Language"],
+        "answer": "Structured Query Language"
+    },
+    {
+        "question": "Which library is used for GUI in Python?",
+        "options": ["NumPy", "Tkinter", "Pandas", "Matplotlib"],
+        "answer": "Tkinter"
+    },
+    {
+        "question":  "Which company developed Python?",
+        "options": ["Microsoft", "Google", "CWI", "Apple"],
+        "answer": "CWI"
+    },
+    {
+        "question": "Which planet is known as Red Planet?",
+        "options": ["Earth", "Mars", "Venus", "Jupiter"],
+        "answer": "Mars"
+    },    
+    {
+        "question": "4 % 0= ?",
+        "options": ["3", "4", "5", "0"],
+        "answer": "0"
+    },
+    
+]
